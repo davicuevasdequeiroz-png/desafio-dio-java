b5a20c4f4ad6cf38ddcbb02e704b3b41d03bd392
Davi Cuevas <davicuevasdequeiroz@gmail.com>
2026-02-17 15:17:12 -0300

    abstração e encapsulamento

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,28 @@
-void main(String[] args) {
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main{
+
+    static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria Java");
+        mentoria.setDescricao("descricao Mentoria de java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1.toString() + curso2.toString());
+        System.out.println(mentoria);
+    }
 
 }
